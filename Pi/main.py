e96baf012e564afd57976c59b4704ee4a4f1afee
acastro <up201108939@up.pt>
2026-05-26 22:59:13 +0100

    added solver_state class to keep track of solution progress, includes methods to move the solution forward and backwards that sends the corresponding move to the arduino through serial

--- Pi/main.py
+++ Pi/main.py
@@ -15,6 +15,76 @@ MAKE_RUBICO = "./Solver/make_rubico.sh"
 PORTA = "/dev/tty.usbmodem101"  #mudar para a porta de usb do raspberry pi
 BAUD_RATE = 115200
 
+back_moves = {
+    "L":    "L'",
+    "L'":   "L" ,
+    "L2":   "L2",
+    "D":    "D'",
+    "D'":   "D" ,
+    "D2":   "D2",
+    "U":    "U'",    
+    "U'":   "U" ,
+    "U2":   "U2",
+    "R":    "R'",  
+    "R'":   "R" ,
+    "R2":   "R2",
+    "B":    "B'",   
+    "B'":   "B" ,
+    "B2":   "B2",
+    "F":    "F'",   
+    "F'":   "F" ,
+    "F2":   "F2",
+    "y":    "y'",
+    "y'":   "y" ,
+    "y2":   "y2",
+    "x":    "x'",
+    "x'":   "x" ,
+    "x2":   "x2",
+    "z":    "z'",      
+    "z'":   "z" ,     
+    "z2":   "z2",     
+}
+
+class solve_state:
+    def __init__(self,arduino_con:ard_con,cube_string:str,solution:str) -> None:
+        self.conn = arduino_con
+        self.cs = cube_string
+        self.sol = solution.split(" ")
+        self.cur_pos = 0
+        self.sol_len = len(self.sol)
+    
+    def forward(self):
+        if self.cur_pos<self.sol_len:
+            self.conn.send_comand(self.sol[self.cur_pos])
+            self.cur_pos += 1
+        else:
+            print("Todos os passos já foram executados\n")
+    
+    def backwards(self):
+        if(self.cur_pos>0): 
+            #undo last move
+            self.cur_pos-=1
+            self.conn.send_comand(back_moves[self.sol[self.cur_pos]])
+        else:
+            print("Já se encontra na posição inicial\n")
+    
+    def fast_solve(self):
+        while(self.cur_pos<self.sol_len):
+            self.forward()
+    
+    def fast_unsolve(self):
+        while(self.cur_pos>0):
+            self.backwards()
+
+    def forward_nsteps(self,steps):
+        end_pos = self.cur_pos+steps
+        while(self.cur_pos<self.sol_len and self.cur_pos<end_pos):
+            self.forward()
+
+    def backwards_nsteps(self,steps):
+        end_pos = self.cur_pos-steps 
+        while(self.cur_pos>0 and self.cur_pos>end_pos):
+            self.backwards
 
 def compile_rubico():
     subprocess.run([MAKE_RUBICO],capture_output=True,
@@ -51,11 +121,16 @@ def main():
         # with canvas(device) as draw:
         #     draw.text((5, 0), "Running Solver..." , fill="white")
         solution = run_solver(cube_string)
+        
+        #cria estado da aplicação
+        state = solve_state(arduino,cube_string,solution)
+
+        #algures por aqui isto tem que ter um loopzito para receber instruções da app
+
+        arduino.close_serial()
     else:
         print("verificar porta de conexão do Arduino\n")
 
-
-    # arduino.close_serial()
 
 if __name__ == "__main__":
     sys.exit(main())
